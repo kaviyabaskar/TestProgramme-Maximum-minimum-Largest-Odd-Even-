5b31a28c7143c717b0d07a8bb58065e1aa276aaf
allsec <allsec@ASTLPTWFH117>
2022-10-13 15:47:08 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace TestProgramme
+{
+    class Program
+    {
+        //entry point of applciation
+        public static void Main(string[] args)
+            {
+            Console.WriteLine("Main Screen");
+            Testpgm checkObj = new Testpgm();
+            // checkObj.DisplayBetweenNumbers();
+            // checkObj.mnn();
+            //checkObj.sum();
+            //checkObj.dollor();
+            // checkObj.largest();
+            checkObj.OddEven1();
+               
+        }
+    }
+}
---
+++ Testpgm.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProgramme
+{
+    internal class Testpgm
+    {
+        //properties
+        int drl;
+        float USD = 82.38f;
+        float result;
+        int min, max, output;
+        int m1, m2, m3, m4;
+        int OddEven;
+        public void OddEven1()
+        {
+            Console.WriteLine("Enter Minimum Value");
+            min = Convert.ToInt32(Console.ReadLine());
+            min = min + 1;
+            Console.WriteLine(" Enter Maximum Value");
+            max = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Even Number");
+            for (int i = min; i < max; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                    i++;
+                }
+            }
+            Console.WriteLine("Odd Number");
+            for (int i = min; i < max; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine(i);
+                    i++;
+                }
+            }
+
+
+
+            /*public void largest()
+             {
+                Console.WriteLine("Enter Number 1:");
+                 m1 = Convert.ToInt32(Console.ReadLine());
+                 Console.WriteLine("Enter Number 2:");
+                 m2= Convert.ToInt32(Console.ReadLine());
+                 Console.WriteLine("Enter Number 3:");
+                 m3 = Convert.ToInt32(Console.ReadLine());
+                 if (m1 > m2)
+                 {
+                     if (m1 > m3)
+                     {
+                         Console.Write($"{m1}is the larger number than {m2} and {m3}");
+                     }
+                     else
+                     {
+                         Console.Write($"{m3}is the larger number than {m2} and {m1}");
+                     }
+                 }
+                 else if (m2 > m3)
+                     Console.Write($"{m2}is the larger number than {m1} and {m3}");
+                 else
+                     Console.Write($"{m3}is the larger number than {m2} and {m1}");
+             }
+         }
+     }*/
+            /* public static void dollor()
+               {
+                   Console.WriteLine("How many dollors do you want to convert into India Rupees");
+                   drl = Convert.ToInt32(Console.ReadLine());
+                   result = drl * USD;
+                   Console.WriteLine($"Today's Rate 1 USD value is:{USD}");
+                   Console.WriteLine($"Anwer:{result}INR");
+               }
+               public void sum()
+               {
+                   Console.WriteLine("Enter Number 1:");
+                 //m1 = Convert.ToInt32(Console.ReadLine());
+                   Console.WriteLine("Enter Number 2:");
+                   //m2= Convert.ToInt32(Console.ReadLine());
+                   Console.WriteLine("Enter Number 3:");
+                  // m3 = Convert.ToInt32(Console.ReadLine());
+                   Console.WriteLine("Emnter Number 4:");
+                   //m4= Convert.ToInt32(Console.ReadLine());
+                   //result = m1 + m2 + m3 + m4;
+                   Console.WriteLine($"The Total Number of 4 Numbers is {result}");
+               }
+               public void mnn()
+               {
+                   Console.WriteLine("Enter A Number:");
+                   min =Convert.ToInt32(Console.ReadLine());
+                   for (int i= 0; i < min; i++)
+                   {
+                       Console.WriteLine($"{i}");
+                   }
+               }
+               //methods
+
+               public void DisplayBetweenNumbers()
+               {
+                   Console.WriteLine(  "Enter Minimum Value");
+                   min = Convert.ToInt32(Console.ReadLine());
+                   min = min+1;
+                   Console.WriteLine(" Enter Maximum Value");
+                   max = Convert.ToInt32(Console.ReadLine());
+                   for (int i = min; i < max; i++)
+                   {
+
+
+                       Console.WriteLine(i);
+                   }
+               }*/
+        }
+    }
+}
+    
